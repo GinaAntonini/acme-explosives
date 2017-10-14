10b318d7f228bf8d769ba9042a9f0fd819f7d8f0
Gina Antonini <ginamantonini@gmail.com>
2017-10-14 09:48:40 -0500

    made it print to console

--- javascripts/data.js
+++ javascripts/data.js
@@ -2,6 +2,8 @@
 
 const dom = require('./dom');
 
+let categories = [];
+let types = [];
 let products = [];
 
 
@@ -17,7 +19,7 @@ const categoriesJSON = () => {
 
 const typesJSON = () => {
 	return new Promise((resolve, reject) => {
-		$.ajax('./db/products.json').done((data2) => {
+		$.ajax('./db/types.json').done((data2) => {
 			resolve(data2.types);
 		}).fail((error2) => {
 			reject(error2);
@@ -35,13 +37,39 @@ const productsJSON = () => {
 	});
 };
 
+var explosivesGetter = () => {
+	categoriesJSON().then((results) => {
+		results.forEach((boom) => {
+			categories.push(boom);
+		});
+		return typesJSON();
+	}).then((results2) => {
+		results2.forEach((boom) => {
+			types.push(boom);
+		});
+		return productsJSON();
+	}).then((results3) => {
+		results3.forEach((boom) => {
+			products.push(boom);
+		});
+		console.log("products", products);
+		}).catch((error) => {
+		console.log('error', error);
+	});
+};
+
+const makeProducts = () => {
+	products.forEach((boom) =>{
+		dom(boom);
+	});
+};
+
 const initializer = () => {
-	console.log("products", products);
-	dom(products);
+	explosivesGetter();
 };
 
-const getProducts = () => {
-	return products;
-};
+// const getProducts = () => {
+// 	return products;
+// };
 
-module.exports = {initializer: initializer, getProducts: getProducts};
+module.exports = {initializer: initializer, explosivesGetter: explosivesGetter};
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -6,8 +6,8 @@ const domString = (product) => {
 	var productString = "";
 		productString += `<div class="col-sm-4">`;
 		productString += `<h1>${product.name}</h1>`;
-		productString += `<h1>${product.type}</h1>`;
-		productString += `<h1>${product.category}</h1>`;
+		// productString += `<h1>${product.type}</h1>`;
+		// productString += `<h1>${product.category}</h1>`;
 		productString += `</div>`;
 	writeToDom(productString);
 };
